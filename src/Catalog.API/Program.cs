f41feafbe680815d4a229351358005aae56e8c25
Gyanendra Rathore <contactgyanendraat@gmail.com>
2025-10-20 06:41:17 +0530

    Catalog Serevie

---
+++ src/Catalog.API/Program.cs
@@ -0,0 +1,41 @@
+using Catalog.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Service to Container
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
+var app = builder.Build();
+
+// Configure HTTP Request Pipeline
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+app.Run();
